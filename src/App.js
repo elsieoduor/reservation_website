02094cab4601188c0fb66428d544b3ca730be1a5
Elsie Oduor <elsie.atieno@student.moringaschool.com>
2023-09-27 09:10:46 +0300

    Created part of project

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import './app.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import List from './pages/List'
+import Hotel from './pages/Hotel'
+
 
 const App = () => {
   return (
-    <div>Hello World!</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/hotels' element={<List/>} />
+        <Route path='/hotels/:id' element={<Hotel/>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
